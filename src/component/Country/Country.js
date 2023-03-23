020e6decf0c0674a99480f48713a17e2fbcdcf26
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-03-24 01:00:13 +0600

    complete to display country

--- src/component/Country/Country.js
+++ src/component/Country/Country.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './Country.css'
 
 const Country = (props) => {
-    const { name, population, region, area } = props.country;
+    const { name, population, region, area, flags } = props.country;
     return (
-        <div>
+        <div className='country'>
+            <img src={flags.png} alt="" />
             <h1>Country name:{name.official}</h1>
             <p>Population: {population}</p>
             <p><small>Region: {region}</small></p>
